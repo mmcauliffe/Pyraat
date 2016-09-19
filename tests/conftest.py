7fae1b6cb8edbcf8a13ddff245daebe43e7f32cc
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-09-19 16:32:27 -0400

    Sanitize praat path

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,6 @@ def sound_file(test_dir):
 @pytest.fixture(scope='module')
 def praatpath():
     if os.environ.get('TRAVIS'):
-        return '$HOME/tools/praat'
+        return os.path.join(os.environ.get('HOME'),'tools','praat')
     return r'C:\Users\michael\Documents\Praat\praatcon.exe'
 
